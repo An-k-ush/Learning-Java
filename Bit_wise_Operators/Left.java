ba2a2dd8dd521a77a8c24677677848fd3fc38252
Ankush <sounankush123@gmail.com>
2023-07-07 15:01:39 +0200

    commit

---
+++ Bit_wise_Operators/Left.java
@@ -0,0 +1,8 @@
+public class Left {
+    public static void main(String args[]) {
+        byte b = 4;
+        int a = b << 2;
+        byte c = (byte)a;
+        System.out.print(a + " " + c);
+    }
+}
