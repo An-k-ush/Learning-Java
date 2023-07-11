f1af74392fdbe00b8ea5b9be01079cbd02dbb5e5
Ankush <sounankush123@gmail.com>
2023-07-11 15:13:26 +0200

    commit

--- 2D-Arrays/Introduction.java
+++ 2D-Arrays/Introduction.java
@@ -5,7 +5,7 @@ public class Introduction {
             int arr2d[][] = new int[2][3];
             // arr2d[2][2] = 3;   
             // System.out.print(arr2d[2][2]); 
-            System.out.println(arr2d.length); // THis will give no of rows
+            System.out.println(arr2d.length); // THis will give no of rows.
             System.out.print("Enter the elements of 2d array : ");
             for(int i = 0; i < arr2d.length; i++) {
                 for(int j = 0; j < arr2d[0].length; j++) {
