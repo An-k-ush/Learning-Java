0277d7fdea18c9694b7a25905c406b8aec203f0d
Ankush <sounankush123@gmail.com>
2023-07-26 08:37:55 +0200

    commit

--- Recursion/Increasing.java
+++ Recursion/Increasing.java
@@ -1,5 +1,15 @@
 public class Increasing {
+    public static void inc(int n) {
+        if(n == 1) {
+            System.out.print(1 + " ");
+            return;
+        }
+        inc(n-1);
+        System.out.print(n + " ");
+        return;
+    }
     public static void main(String args[]) {
-        
+        int n = 5;
+        inc(n);
     }
 }
